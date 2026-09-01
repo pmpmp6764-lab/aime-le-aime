9987def3c98baef68e458a50401a2791f2babeb2
pmpmp6764-lab <pmpmp6764@gmail.com>
2026-09-01 15:10:13 +0000

    add Windows launcher 啟動.vbs and 啟動.bat

--- 啟動.bat
+++ 啟動.bat
@@ -1,10 +1,21 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-if not exist "index.html" (
-  echo 找不到 index.html，請先把壓縮檔完整解壓。
+if not exist "%~dp0index.html" (
+  echo 找不到 index.html，請把啟動程式和網頁放在同一資料夾。
   pause
   exit /b 1
 )
-start "" "index.html"
-exit
+if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
+  start "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --app="%~dp0index.html"
+  exit /b 0
+)
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
+  start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app="%~dp0index.html"
+  exit /b 0
+)
+if exist "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" (
+  start "" "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" --app="%~dp0index.html"
+  exit /b 0
+)
+start "" "%~dp0index.html"
